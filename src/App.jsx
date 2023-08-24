37bb16b86985d84a5d2f8099684c45cc7eafbc40
johnvdpk <johnvdpk@gmail.com>
2023-08-24 17:00:34 +0200

    Css weer ophalen

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import './App.css';
+import './constants/inventory.js';
+import './helpers/Sold.js';
+import sold from "./helpers/Sold.js";
 
 function App() {
+
   return (
+      <body>
     <h1>Begin hier met met maken van de applicatie!</h1>
+
+        <div class="sold">
+        <p>Er zijn in totaal {sold()} Tv's verkocht</p>
+        </div>
+      </body>
   )
 }
 
